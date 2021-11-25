9774daf0df7eb0ed1dbda5d01d4e5c35f88073be
Tomasz Cekało <tomasz.cekalo@gmail.com>
2021-11-25 12:28:10 +0100

    added simple parsing, need create some structure for this code and see if units can be split from numbers

---
+++ BiznesradarPLScraper.Console/Program.cs
@@ -0,0 +1,7 @@
+﻿
+using BiznesradarPLScraper;
+
+var task = await new DywidendyScraper()
+    .Scrape("https://www.biznesradar.pl/dywidendy/indeks:WIGdiv,2021,3,2");
+
+Console.WriteLine("done");
--- BiznesradarPLScraper/DywidendyScraper.cs
+++ BiznesradarPLScraper/DywidendyScraper.cs
@@ -1,12 +1,63 @@
-﻿using System;
+﻿using AngleSharp;
+using AngleSharp.Dom;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace BiznesradarPLScraper
 {
     public class DywidendyScraper
     {
+        public async Task<object> Scrape(string url)
+        {
+            var config = Configuration.Default.WithDefaultLoader();
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync(url);
+            var dywidendy = ParseDywidendy(document);
+            return dywidendy;
+        }
+
+        private object ParseDywidendy(IDocument document)
+        {
+            var rows = document
+                .QuerySelectorAll("table.qTableFull tr")
+                .Select(x => ParseRow(x))
+                .ToList();
+            return rows;
+        }
+
+        private object ParseRow(IElement x)
+        {
+            var cells = x.QuerySelectorAll("td");
+            if(cells.Count()>0)
+            {
+                var result = new
+                {
+                    Profil=cells[0],
+                    Rok = cells[1]
+                        .TextContent,
+                    DataWZA=cells[2]
+                        .TextContent,
+                    LastDay=cells[3]
+                        .TextContent,
+                    PayoutDay=cells[4]
+                        .TextContent,
+                    PerStock=cells[5]
+                        .TextContent
+                        .Trim(),
+                    Percentage=cells[6]
+                        .TextContent
+                        .Trim(),
+                    Status=cells[7]
+                        .TextContent
+                        .Trim()
+                };
+                return result;
+            }
+            return null; 
+        }
     }
 }
